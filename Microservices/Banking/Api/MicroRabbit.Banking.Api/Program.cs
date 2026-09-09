cddb20e94aad1a24dcb1b063ba4204eadf74ea61
Oleksandr Chaban <oleksandrchabanm@gmail.com>
2026-09-09 18:29:04 +0300

    Api setup and Banking Controller

--- Microservices/Banking/Api/MicroRabbit.Banking.Api/Program.cs
+++ Microservices/Banking/Api/MicroRabbit.Banking.Api/Program.cs
@@ -1,8 +1,35 @@
+using MicroRabbit.Banking.Data.Context;
+using MicroRabbit.Infra.IoC;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddMediatR((config) =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
+
+builder.Services.AddSwaggerGen((setup) =>
+{
+    setup.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Banking Microservice",
+        Version = "v1"
+    });
+});
+
+DependencyContainer.RegisterServices(builder.Services);
+
+builder.Services.AddDbContext<BankingDbContext>((options) =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("BankingDbConnection");
+    options.UseSqlServer(connectionString);
+});
 
 var app = builder.Build();
 
@@ -11,6 +38,11 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
+
+app.UseSwaggerUI((options) =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "Banking Microservice v1");
+});
 
 app.UseHttpsRedirection();
 
